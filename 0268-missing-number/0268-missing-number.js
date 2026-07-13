d5a435da431b89736c3f95bec8c9a20ec749a40e
yash kumar <155902141+yashTomar272@users.noreply.github.com>
2026-07-14 00:55:55 +0530

    Time: 0 ms (100%), Space: 56.1 MB (69.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,13 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-   let set=new Set()
-for(let ch of nums){
-    set.add(ch)
-}
-for(let i=0;i<=nums.length;i++){
-    if(!set.has(i)){
-        return i
-    }
-} 
+ let n=nums.length
+ let totalSum=n*(n+1)/2
+ let realSum=0
+ for(let i=0;i<n;i++){
+    realSum+=nums[i]
+ }
+ return totalSum-realSum
 };
