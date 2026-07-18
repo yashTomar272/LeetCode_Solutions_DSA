37fcc6222e37dc48f391fd6945585660a490c5ce
yash kumar <155902141+yashTomar272@users.noreply.github.com>
2026-07-18 10:36:23 +0530

    Time: 2 ms (64.56%), Space: 55.3 MB (71.32%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let i=0
+let j=numbers.length-1
+
+while(i<j){
+  let sum=numbers[i]+numbers[j]
+  if(sum<target) i++
+  else if(sum==target) return [i+1,j+1]
+  else j--
+} 
+};
