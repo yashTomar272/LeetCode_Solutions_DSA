bcb30b8dca274cce784952e7e6f9be5a6b80fc32
yash kumar <155902141+yashTomar272@users.noreply.github.com>
2026-07-30 23:29:03 +0530

    Time: 51 ms (13.3%), Space: 53.8 MB (40.69%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    let result=""
+   for(let ch of address){
+      if(ch=="."){
+         result+="[.]"
+      }else{
+         result+=ch
+      }
+   }
+   return result
+};
