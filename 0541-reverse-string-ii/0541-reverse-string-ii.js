e6039ef7eb5a7720a5a8dbc9a0a28af9c693fe90
yash kumar <155902141+yashTomar272@users.noreply.github.com>
2026-08-01 00:17:14 +0530

    Time: 6 ms (32.83%), Space: 58 MB (21.67%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+     let arr = s.split("")
+
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let left = i
+        let right = Math.min(i + k - 1, arr.length - 1)
+
+        while (left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]]
+            left++
+            right--
+        }
+    }
+
+    return arr.join("")
+};
